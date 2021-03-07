82e58368684ac397ba568adc5d23480329495742
Emily Nixon <enixon@qa.com>
2021-03-07 17:20:16 +0000

    Updated error in villager (birthSeason)

--- domain/Villager.java
+++ domain/Villager.java
@@ -16,7 +16,7 @@ public class Villager {
 
 	private int birthDay;
 
-	private String birthMonth;
+	private String birthSeason;
 
 	private String faveItem;
 
@@ -26,21 +26,21 @@ public class Villager {
 		super();
 	}
 
-	public Villager(Long id, String name, int birthDay, String birthMonth, String faveItem, String leastFaveItem) {
+	public Villager(Long id, String name, int birthDay, String birthSeason, String faveItem, String leastFaveItem) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.birthDay = birthDay;
-		this.birthMonth = birthMonth;
+		this.birthSeason = birthSeason;
 		this.faveItem = faveItem;
 		this.leastFaveItem = leastFaveItem;
 	}
 
-	public Villager(String name, int birthDay, String birthMonth, String faveItem, String leastFaveItem) {
+	public Villager(String name, int birthDay, String birthSeason, String faveItem, String leastFaveItem) {
 		super();
 		this.name = name;
 		this.birthDay = birthDay;
-		this.birthMonth = birthMonth;
+		this.birthMonth = birthSeason;
 		this.faveItem = faveItem;
 		this.leastFaveItem = leastFaveItem;
 	}
@@ -69,12 +69,12 @@ public class Villager {
 		this.birthDay = birthDay;
 	}
 
-	public String getBirthMonth() {
-		return birthMonth;
+	public String getBirthSeason() {
+		return birthSeason;
 	}
 
-	public void setBirthMonth(String birthMonth) {
-		this.birthMonth = birthMonth;
+	public void setBirthSeason(String birthSeason) {
+		this.birthSeason = birthSeason;
 	}
 
 	public String getFaveItem() {
@@ -98,7 +98,7 @@ public class Villager {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + birthDay;
-		result = prime * result + ((birthMonth == null) ? 0 : birthMonth.hashCode());
+		result = prime * result + ((birthSeason == null) ? 0 : birthSeason.hashCode());
 		result = prime * result + ((faveItem == null) ? 0 : faveItem.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((leastFaveItem == null) ? 0 : leastFaveItem.hashCode());
@@ -117,10 +117,10 @@ public class Villager {
 		Villager other = (Villager) obj;
 		if (birthDay != other.birthDay)
 			return false;
-		if (birthMonth == null) {
-			if (other.birthMonth != null)
+		if (birthSeason == null) {
+			if (other.birthSeason != null)
 				return false;
-		} else if (!birthMonth.equals(other.birthMonth))
+		} else if (!birthSeason.equals(other.birthSeason))
 			return false;
 		if (faveItem == null) {
 			if (other.faveItem != null)
@@ -147,7 +147,7 @@ public class Villager {
 
 	@Override
 	public String toString() {
-		return "Villager [id=" + id + ", name=" + name + ", birthDay=" + birthDay + ", birthMonth=" + birthMonth
+		return "Villager [id=" + id + ", name=" + name + ", birthDay=" + birthDay + ", birthSeason=" + birthSeason
 				+ ", faveItem=" + faveItem + ", leastFaveItem=" + leastFaveItem + "]";
 	}
 
